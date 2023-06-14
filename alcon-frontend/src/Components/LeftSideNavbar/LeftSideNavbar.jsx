860c586eeb63438b9061922ee0dccbef10f18442
2manishlanghe <2manishlanghe@gmail.com>
2023-06-14 17:31:30 +0530

    Edited Aside bar

--- alcon-frontend/src/Components/LeftSideNavbar/LeftSideNavbar.jsx
+++ alcon-frontend/src/Components/LeftSideNavbar/LeftSideNavbar.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function LeftSideNavbar() {
-  return (
-    <div>LeftSideNavbar</div>
-  )
+  return <div>New LeftSideNavbar</div>;
 }
