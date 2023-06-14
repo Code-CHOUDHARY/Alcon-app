ad27a87c79227adb2d990febdd52f339559180fb
Code-Choudhary <devkaransingh928@gmail.com>
2023-06-14 15:28:44 +0530

    Added folder structure

--- alcon-frontend/src/App.js
+++ alcon-frontend/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      
-    </div>
+    <h1>Hello world</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <button class="bg-sky-500 hover:bg-sky-700 ...">
+  Save changes
+</button>
+  </div>
   );
 }
 
