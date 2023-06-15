b841087e4a0bc7c1dfa2e2bc867c90c71ffb8317
Code-Choudhary <devkaransingh928@gmail.com>
2023-06-15 14:46:40 +0530

    added post view card

--- alcon-frontend/src/Pages/Dashboard/Dashboard.jsx
+++ alcon-frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -3,12 +3,14 @@ import Topnavbar from '../../Components/Topnavbar/Topnavbar'
 import LeftSideNavbar from '../../Components/LeftSideNavbar/LeftSideNavbar'
 import Home from '../../Components/Home/Home'
 import RightSideNavbar from '../../Components/RightSideNavbar/RightSideNavbar'
+import ViewPostCard from '../../Components/Post/ViewPostCard'
 export default function Dashboard() {
   return (
     <div>
       <Topnavbar/>
       <LeftSideNavbar/>
       <Home/>
+      <ViewPostCard/>
       <RightSideNavbar/>
     </div>
   )
