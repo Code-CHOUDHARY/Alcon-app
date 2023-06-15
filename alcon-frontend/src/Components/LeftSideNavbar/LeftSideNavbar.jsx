7758df5238d4dc5a78239b82ca4e62b27483e2a2
2manishlanghe <2manishlanghe@gmail.com>
2023-06-15 14:41:04 +0530

    Added left Navbar

--- alcon-frontend/src/Components/LeftSideNavbar/LeftSideNavbar.jsx
+++ alcon-frontend/src/Components/LeftSideNavbar/LeftSideNavbar.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Menu } from "./SidebarConfig";
+const LeftSideNavbar = () => {
+  const [activetab, setActiveTab] = useState();
+  const navigate = useNavigate();
+  const handleTabClick = (title) => {
+    setActiveTab(title);
+    if (title === "profile") {
+      navigate("/username");
+    } else if (title === "home") {
+      navigate("/");
+    }
+  };
 
-export default function LeftSideNavbar() {
-  return <div>New LeftSideNavbar</div>;
-}
+  return (
+    <div className="sticky top-0">
+      <div>
+        <h3 className="items-center">Alcon</h3>
+        {/* <img className="w-20" src="./assets/images/alcon.svg" alt="alcon" /> */}
+      </div>
+
+      <div className="mt-20 ">
+        {Menu.map((item) => (
+          <div
+            onClick={() => handleTabClick(item.title)}
+            className="flex items-center mb-5 cursor-pointer text-lg"
+          >
+            <p className="ml-5 p-2">
+              {activetab === item.title ? item.activeIcon : item.icon}
+            </p>
+            <p>{item.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideNavbar;
